b56181f94a63d20503bbc57ba79627158d8b36c3
azizbekeshonaliyev <1996azizbekeshonaliyev@gmail.com>
2020-07-12 11:35:02 +0500

    create questions

--- src/views/Question/QuestionList.js
+++ src/views/Question/QuestionList.js
@@ -1,23 +1,61 @@
 import React, { Component } from 'react'
 import QuestionItem from './QuestionItem';
 import RightMenu from './RightMenu';
-
+import Create from './Create';
+import uuid from  'uuid';
 export default class QuestionList extends Component {
     constructor(props) {
         super(props);
-        let item = {
-            title: '10 Kids Unaware of Their Halloween Costume',
-            desc: "It's one thing to subject yourself to a Halloween costume mishap because, hey, that's your prerogative."
+        this.state = { 
+            questions: [],
+            title: '',
+            desc: '',
+            id: uuid()
         };
-        this.state = { questions: Array(20).fill(item) };
+    }
+    handleTitleChange = e => {
+        this.setState({
+            title: e.target.value
+        })
+    }
+    handleDescChange = e => {
+        this.setState({
+            desc: e.target.value
+        })
+    }
+    handleSubmit = e => {
+        e.preventDefault()
+
+        const item = {
+            id:this.state.id,
+            title: this.state.title,
+            desc: this.state.desc
+        }
+
+        const items = [...this.state.questions,item];
+
+        this.setState({
+            questions: items,
+            title: '',
+            desc: '',
+            id: uuid()
+        })
     }
     render() {
         return (
 	        <div className="row pt-4">
                 <div className="col-12 col-sm-12 col-md-9">
                     {this.state.questions.map(item => (
-                        <QuestionItem key={item.title} question={item}></QuestionItem>
+                        <QuestionItem key={item.id} question={item}></QuestionItem>
                     ))}
+
+                    <Create 
+                        submit={this.handleSubmit} 
+                        handleTitleChange={this.handleTitleChange}
+                        handleDescChange={this.handleDescChange}
+                        title={this.state.title}
+                        desc={this.state.desc}
+                    ></Create>
                 </div>
                 <RightMenu></RightMenu>                    
             </div>
